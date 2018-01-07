e17c47156c4400575c3bf526f6e2cde842dafbc6
tunde Oretade <tundeiness@yahoo.co.uk>
2018-01-07 22:55:14 +0000

    automatically backed up by learn

--- objects.js
+++ objects.js
@@ -3,7 +3,7 @@ var playlist = new Object({
   Bonjovi: 'oatmeal'
   });
   
-  function updatePlayList(playlist, artisteName, songTitle){
+  function updatePlayList(playlist, artistName, songTitle){
     return Object.assign({}, playlist,{[artisteName]: songTitle});
   
   }
